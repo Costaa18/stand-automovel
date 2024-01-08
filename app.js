829c07215fa1134414db2c33cc25b03f1321a932
Costa <67383500+Costaa18@users.noreply.github.com>
2024-01-08 21:47:37 +0000

    Finalização do Trabalho de Programação Web I

--- app.js
+++ app.js
@@ -1,96 +1,15 @@
-let listaCarros = JSON.parse(localStorage.getItem('listaCarros')) || [];
-
-const listaCarrosArray = [
-  {
-    id: 1,
-    modelo: "E-type",
-    marca: "Jaguar",
-    ano: 1978,
-    preco: "1.500.000€",
-    combustivel: "gasolina",
-    km: "10km",
-    img: "assets/img/cars/jaguar.jpg",
-    img2: "assets/img/cars/jaguar2.jpg",
-    img3: "assets/img/cars/jaguar-etype.png",
-    img4: "assets/img/cars/jaguar3.webp",
-    lugares: "5 pessoas",
-    stand: "Stand Famalicao",
-    local: "Nova Iorque, EUA",
-    standImg: "assets/img/cars/car-list-icon-01.png",
-    estado: "disponivel",
-    link: "jaguare-type.html"
-  },
-  {
-    id: 2,
-    modelo: "Dawn Drophead 1949",
-    marca: "Rolls Royce",
-    ano: 1949,
-    preco: "7.800.000€",
-    combustivel: "gasolina",
-    km: "10km",
-    img: "assets/img/cars/rolls-royce.jpg",
-    img2: "assets/img/cars/rolls-royce2.webp",
-    img3: "assets/img/cars/rolls-royce3.avif",
-    img4: "assets/img/cars/rolls-royce4.jpg",
-    lugares: "5 pessoas",
-    stand: "Auto Mobilitcs",
-    local: "Nova Iorque, EUA",
-    standImg: "assets/img/cars/car-list-icon-02.png",
-    estado: "disponivel",
-    link: "rolls-royce.html"
-  },
-  {
-    id: 3,
-    modelo: "365 Speedster 1954",
-    marca: "Porsche",
-    ano: 1954,
-    preco: "4.350.000€",
-    combustivel: "gasolina",
-    km: "10km",
-    img: "assets/img/cars/porsche-spyder.jpg",
-    img2: "assets/img/cars/porsche2.jpg",
-    img3: "assets/img/cars/porsche3.jpg",
-    img4: "assets/img/cars/porsche4.jpg",
-    lugares: "5 pessoas",
-    stand: "Auto Mobilitcs",
-    local: "Nova Iorque, EUA",
-    standImg: "assets/img/cars/car-list-icon-02.png",
-    estado: "indisponivel",
-    link : "porsche.html"
-  },
-  {
-    id: 4,
-    modelo:"300 SL Gullwing 1957",
-    marca: "Mercedes",
-    ano: 1957,
-    preco: "16.350.000€",
-    combustivel: "gasolina",
-    km: "10km",
-    img: "assets/img/cars/mercedes.jpg",
-    img2: "assets/img/cars/mercedes2.webp",
-    img3: "assets/img/cars/mercedes3.jpg",
-    img4: "assets/img/cars/mercedes4.webp",
-    lugares: "5 pessoas",
-    stand: "Auto Docs",
-    local: "Nova Iorque, EUA",
-    standImg: "assets/img/cars/car-list-icon-03.png",
-    estado: "disponivel",
-    link:"mercedes.html"
-  }
-];
-localStorage.setItem('listaCarros', JSON.stringify(listaCarrosArray));
+let listaCarros = JSON.parse(localStorage.getItem("listaCarros")) || [];
 
 console.log(listaCarros);
-console.log(localStorage.getItem('listaCarros'));
-
+console.log(localStorage.getItem("listaCarros"));
 
 function generateCarListings() {
-    const carListingsContainer = document.getElementById('carListings');
+  const carListingsContainer = document.getElementById("carListings");
 
-    listaCarros.forEach((car, index) => {
-      const listingElement = document.createElement('div');
-      listingElement.classList.add('listview-car');
-      listingElement.innerHTML = `
+  listaCarros.forEach((car, index) => {
+    const listingElement = document.createElement("div");
+    listingElement.classList.add("listview-car");
+    listingElement.innerHTML = `
         <div class="card">
           <div class="blog-widget d-flex">
             <div class="blog-img">
@@ -107,7 +26,9 @@ function generateCarListings() {
                          <h6>Categoria : <span>${car.marca}</span></h6>		
                      </div>
                      <div class="blog-list-rate">
-  <span class="custom-badge ${car.estado === 'disponivel' ? 'custom-badge-success' : 'custom-badge-danger'}">${car.estado === 'disponivel' ? 'Disponível' : 'Indisponível'}</span>
+  <span class="custom-badge ${
+    car.estado === "disponivel" ? "custom-badge-success" : "custom-badge-danger"
+  }">${car.estado === "disponivel" ? "Disponível" : "Indisponível"}</span>
   <h6>${car.preco}<span></span></h6>
 </div>
                  </div>	
@@ -118,11 +39,15 @@ function generateCarListings() {
                              <p>Auto</p>
                          </li>
                          <li>
-                             <span><img src="assets/img/icons/car-parts-02.svg" alt="${car.km}"></span>
+                             <span><img src="assets/img/icons/car-parts-02.svg" alt="${
+                               car.km
+                             }"></span>
                              <p>${car.km}</p>
                          </li>
                          <li>
-                             <span><img src="assets/img/icons/car-parts-03.svg" alt="${car.combustivel}"></span>
+                             <span><img src="assets/img/icons/car-parts-03.svg" alt="${
+                               car.combustivel
+                             }"></span>
                              <p>${car.combustivel}</p>
                          </li>
                          <li>
@@ -130,7 +55,9 @@ function generateCarListings() {
                              <p>AWD</p>
                          </li>
                          <li>
-                             <span><img src="assets/img/icons/car-parts-05.svg" alt="${car.ano}"></span>
+                             <span><img src="assets/img/icons/car-parts-05.svg" alt="${
+                               car.ano
+                             }"></span>
                              <p>${car.ano}</p>	
                          </li>
                          <li>
@@ -147,12 +74,16 @@ function generateCarListings() {
                              </div>
                              <div class="address-info">
                                  <h5><a href="#">Lincoln Park</a></h5>
-                                 <h6><i class="feather-map-pin me-2"></i>${car.local}</h6>
+                                 <h6><i class="feather-map-pin me-2"></i>${
+                                   car.local
+                                 }</h6>
                              </div>
                          </div>	
                      </div>
                      <div class="listing-button">
-                     <a href="reservar.html?id=${car.id}" class="btn btn-order" id="reserveButton">
+                     <a href="reservar.html?id=${
+                       car.id
+                     }" class="btn btn-order" id="reserveButton">
                         <span><i class="feather-calendar me-2"></i></span>Reservar Agora
                     </a>
                      </div>
@@ -162,36 +93,32 @@ function generateCarListings() {
         </div>
       `;
 
-
-      const reserveButton = listingElement.querySelector('.btn-order');
+    const reserveButton = listingElement.querySelector(".btn-order");
     reserveButton.dataset.carIndex = index;
-    reserveButton.addEventListener('click', handleReserveButtonClick);
-  
-  
-      carListingsContainer.appendChild(listingElement);
+    reserveButton.addEventListener("click", handleReserveButtonClick);
+
+    carListingsContainer.appendChild(listingElement);
+  });
+}
+
+generateCarListings();
+
+function handleReserveButtonClick(event) {
+  event.preventDefault();
+
+  const carIndex = event.currentTarget.dataset.carIndex;
+  const car = listaCarros[carIndex];
+
+  if (car.estado === "disponivel") {
+    window.location.href = `reservar.html?id=${car.id}`;
+  } else {
+    Swal.fire({
+      icon: "warning",
+      title: "Carro Indisponível",
+      text: "Desculpe, este carro não está disponível para reserva.",
+      confirmButtonColor: "#3085d6",
+      confirmButtonText: "Ok",
     });
   }
-
-  generateCarListings();
-
-
-  function handleReserveButtonClick(event) {
-    event.preventDefault(); 
-  
-    const carIndex = event.currentTarget.dataset.carIndex;
-    const car = listaCarros[carIndex];
-  
-    if (car.estado === 'disponivel') {
-      
-      window.location.href = `reservar.html?id=${car.id}`;
-    } else {
-        
-      Swal.fire({
-        icon: 'warning',
-        title: 'Carro Indisponível',
-        text: 'Desculpe, este carro não está disponível para reserva.',
-        confirmButtonColor: '#3085d6',
-        confirmButtonText: 'Ok'
-      });
-    }
 }
+
--- reservar.js
+++ reservar.js
@@ -1,30 +1,130 @@
- // Retrieve the car list from local storage
- const storedCarList = localStorage.getItem('listaCarros');
- const listaCarros = JSON.parse(storedCarList);
+const storedCarList = localStorage.getItem("listaCarros");
+const listaCarros = JSON.parse(storedCarList);
 
- // Get the car ID from the URL
- const urlParams = new URLSearchParams(window.location.search);
- const carId = parseInt(urlParams.get('id'));
+const urlParams = new URLSearchParams(window.location.search);
+const carId = parseInt(urlParams.get("id"));
 
- // Find the car with the matching ID
- const selectedCar = listaCarros.find(car => car.id === carId);
- console.log(selectedCar);
+const selectedCar = listaCarros.find((car) => car.id === carId);
+//console.log(selectedCar);
 
- // Now you can use the selectedCar object to display information on the page
- if (selectedCar) {
-   // Example: Display car information in the document
-   document.getElementById('tituloCarro').innerText = selectedCar.marca + " " + selectedCar.modelo;
-   document.getElementById('anoCarro').innerText = selectedCar.ano;
-   document.getElementById('nomeCompletoCarro').innerText = selectedCar.marca + " " + selectedCar.modelo;
-    document.getElementById('localizacaoSpan').innerHTML = "<b> Localização </b> : " + selectedCar.local;
+const listaReservasArray = [];
 
-    document.getElementById('carImg1').src = selectedCar.img;
-    document.getElementById('carThumbnail1').src = selectedCar.img;
-    document.getElementById('carImg2').src = selectedCar.img2;
-    document.getElementById('carThumbnail2').src = selectedCar.img2;
-    document.getElementById('carImg3').src = selectedCar.img3;
-    document.getElementById('carThumbnail3').src = selectedCar.img3;
-    document.getElementById('carImg4').src = selectedCar.img4;
-    document.getElementById('carThumbnail4').src = selectedCar.img4;
-   // Add more fields as needed
- }
+if (selectedCar) {
+  document.getElementById("tituloCarro").innerText =
+    selectedCar.marca + " " + selectedCar.modelo;
+
+  document.getElementById("anoCarro").innerText = selectedCar.ano;
+  document.getElementById("anoCarroDetalhes").innerHTML = selectedCar.ano;
+
+  document.getElementById("nomeCompletoCarro").innerText =
+    selectedCar.marca + " " + selectedCar.modelo;
+
+  document.getElementById("localizacaoSpan").innerHTML =
+    "<b> Localização </b> : " + selectedCar.local;
+
+  document.getElementById("kmCarroDetalhes").innerHTML = selectedCar.km;
+
+  document.getElementById("combustivelCarroDetalhes").innerHTML =
+    selectedCar.combustivel;
+
+  document.getElementById("marcaCarroDetalhes").innerHTML = selectedCar.marca;
+
+  document.getElementById("carImg1").src = selectedCar.img;
+  document.getElementById("carThumbnail1").src = selectedCar.img;
+  document.getElementById("carImg2").src = selectedCar.img2;
+  document.getElementById("carThumbnail2").src = selectedCar.img2;
+  document.getElementById("carImg3").src = selectedCar.img3;
+  document.getElementById("carThumbnail3").src = selectedCar.img3;
+  document.getElementById("carImg4").src = selectedCar.img4;
+  document.getElementById("carThumbnail4").src = selectedCar.img4;
+
+  document.getElementById("standCarro").innerText = selectedCar.stand;
+} else if (selectedCar.estado == "indisponivel" || selectedCar.estado == null) {
+  window.location.href = "index.html";
+} else {
+  window.location.href = "index.html";
+}
+
+function checkDisponibilidade() {
+  // Obter os dados do formulário
+  const nomeCompleto = document.querySelector(
+    '#reservaForm input[name="nomeCompleto"]'
+  ).value;
+  const telefone = document.querySelector(
+    '#reservaForm input[name="telefone"]'
+  ).value;
+  const localRecolha = document.querySelector(
+    '#reservaForm input[name="localRecolha"]'
+  ).value;
+  const localEntrega = document.querySelector(
+    '#reservaForm input[name="localEntrega"]'
+  ).value;
+  const dataRecolha = document.querySelector(
+    '#reservaForm input[name="dataRecolha"]'
+  ).value;
+  const horaRecolha = document.querySelector(
+    '#reservaForm input[name="horaRecolha"]'
+  ).value;
+  const dataEntrega = document.querySelector(
+    '#reservaForm input[name="dataEntrega"]'
+  ).value;
+  const horaEntrega = document.querySelector(
+    '#reservaForm input[name="horaEntrega"]'
+  ).value;
+
+  // Obter a lista de reservas existente da localStorage
+  const storedReservas = localStorage.getItem("listaReservas");
+  const listaReservasArray = storedReservas ? JSON.parse(storedReservas) : [];
+
+  // Verificar disponibilidade
+  if (selectedCar.estado === "disponivel") {
+    // Adicionar reserva à lista
+    const reserva = {
+      idCarro: selectedCar.id,
+      nomeCompleto,
+      telefone,
+      localRecolha,
+      localEntrega,
+      dataRecolha,
+      horaRecolha,
+      dataEntrega,
+      horaEntrega,
+    };
+
+    listaReservasArray.push(reserva);
+
+    // Atualizar estado do carro para indisponível
+    selectedCar.estado = "indisponivel";
+
+    // Atualizar localStorage
+    localStorage.setItem("listaCarros", JSON.stringify(listaCarros));
+    localStorage.setItem("listaReservas", JSON.stringify(listaReservasArray));
+
+    // Mensagem de sucesso
+    Swal.fire({
+      icon: "success",
+      title: "Reserva Efetuada",
+      text: "Sua reserva foi efetuada com sucesso!",
+      confirmButtonColor: "#3085d6",
+      confirmButtonText: "Ok",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        window.location.href = "lista-carros.html";
+      }
+    });
+  } else {
+    // Mensagem de indisponibilidade
+    Swal.fire({
+      icon: "warning",
+      title: "Carro Indisponível",
+      text: "Desculpe, este carro não está disponível para reserva.",
+      confirmButtonColor: "#3085d6",
+      confirmButtonText: "Ok",
+    });
+  }
+}
+
+// Adicionar evento de clique ao botão de Verificar Disponibilidade
+document
+  .querySelector(".check-available")
+  .addEventListener("click", checkDisponibilidade);
